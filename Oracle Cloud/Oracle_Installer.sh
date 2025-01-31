0109f4c258aee107a1ce7c3ee40b0388e3c1b262
Lukas Strutz <35605744+AntonMC-Github@users.noreply.github.com>
2025-01-31 02:19:18 +0100

    Update Oracle_Installer.sh

--- Oracle Cloud/Oracle_Installer.sh
+++ Oracle Cloud/Oracle_Installer.sh
@@ -203,7 +203,7 @@ create_server_config () {
   echo -e "${GREEN}Wireguard Config file created at $WGCONFLOC${NC}"
   echo ""
   echo -en "${YELLOW}Starting Wireguard${NC}..."
-  wg-quick up wgo
+  wg-quick up wg0
   echo -e "[${GREEN}Done${NC}]"
   echo -e "${YELLOW}Waiting for connection${NC}..."
   while ! ping -c 1 -W 1 $WG_CLIENT_IP > /dev/null 2>&1; do
@@ -212,7 +212,7 @@ create_server_config () {
   done
   echo -e "[${GREEN}Connection Established${NC}]"
   echo -en "${YELLOW}Enabling Wireguard to start across reboots${NC}..."
-  wg-quick up wgo
+  wg-quick up wg0
   echo -e "[${GREEN}Done${NC}]"
   echo "Your wireguard tunnel should be set up now.  If you need to reset the link for any reason, please run 'systemctl reboot wg-quick@wg0'"
 }
@@ -263,7 +263,7 @@ create_client_config () {
   echo -e "[${GREEN}Connection Established${NC}]"
   echo ""
   echo -en "${YELLOW}Enabling Wireguard to start across reboots${NC}..."
-  wg-quick up wgo
+  wg-quick up wg0
   echo -e "[${GREEN}Done${NC}]"
 }
 
@@ -379,7 +379,7 @@ ask_firewall () {
 
 start_wireguard () {
   echo -en "${YELLOW}Starting wireguard services${NC}..."
-  wg-quick up wgo
+  wg-quick up wg0
   echo -e "[${GREEN}Done${NC}]"
 }
 
