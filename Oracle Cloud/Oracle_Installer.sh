fadbfde42bacd20b2208e953dcf49716bdb2044e
Lukas Strutz <35605744+AntonMC-Github@users.noreply.github.com>
2025-01-31 02:28:55 +0100

    Update Oracle_Installer.sh

--- Oracle Cloud/Oracle_Installer.sh
+++ Oracle Cloud/Oracle_Installer.sh
@@ -125,7 +125,7 @@ create_keys () {
   
   if [[ -f "$PRIVATE_KEY_FILE" && -f "$PUBLIC_KEY_FILE" ]]; then
     echo -e "[${GREEN}Found existing keys${NC}]"
-    sudo cp "$PRIVATE_KEY_FILE" "$WGCONFLOC"
+    # sudo cp "$PRIVATE_KEY_FILE" "$WGCONFLOC"
     sudo cp "$PUBLIC_KEY_FILE" "$WGPUBKEY"
   else
     echo -e "[${YELLOW}No existing keys found, generating new keys${NC}]"
